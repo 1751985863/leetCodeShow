807f4ceb105440d6e947dba440cb12b45a371c08
1751985863 <50584716+1751985863@users.noreply.github.com>
2025-10-04 23:53:49 +0800

    53. 最大子数组和 - 力扣（LeetCode） https://leetcode.cn/problems/maximum-subarray/solutions/3623353/mian-shi-ban-by-insorker-egrr/

--- src/leetcode/editor/cn/Solution.java
+++ src/leetcode/editor/cn/Solution.java
@@ -1,7 +1,6 @@
 package leetcode.editor.cn;
 
-import java.util.HashMap;
-import java.util.PriorityQueue;
+import java.util.*;
 
 class Solution {
     public static int lengthOfLongestSubstring(String s) {
@@ -45,7 +44,45 @@ class Solution {
         return priorityQueue.peek();
     }
 
+    public static List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> result = new ArrayList<>();
+        for (int i = 0; i <= nums.length - 3; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            int left = i+1, right = nums.length - 1;
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum == 0) {
+                    result.add(Arrays.asList(nums[i],nums[left],nums[right]));
+                    while (left < nums.length - 1 && nums[left] == nums[left + 1]) {
+                        left++;
+                    }
+                    while (right > i && nums[right] == nums[right - 1]) {
+                        right--;
+                    }
+                    left++;
+                    right--;
+                } else if (sum > 0) {
+                    right--;
+                } else {
+                    left++;
+                }
+            }
+        }
+        return result;
+    }
+
+    public int maxSubArray(int[] nums) {
+        int ans = Integer.MIN_VALUE;
+        int f = 0;
+        for (int x : nums) {
+            f = Math.max(f,0) + x;
+            ans = Math.max(ans,f);
+        }
+        return ans;
+    }
+
     public static void main(String[] args) {
-        int abba = lengthOfLongestSubstring("abba");
+        List<List<Integer>> lists = threeSum(new int[]{0, 0, 0});
     }
 }

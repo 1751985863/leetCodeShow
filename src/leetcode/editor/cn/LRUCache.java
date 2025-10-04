5c0e08fa183092b069fe78f559fe7658bd6876c9
1751985863 <50584716+1751985863@users.noreply.github.com>
2025-10-04 20:49:46 +0800

    146. LRU 缓存 https://leetcode.cn/problems/lru-cache/description/

---
+++ src/leetcode/editor/cn/LRUCache.java
@@ -0,0 +1,33 @@
+package leetcode.editor.cn;
+
+import java.util.LinkedHashMap;
+
+class LRUCache {
+
+    LinkedHashMap<Integer,Integer> map;
+    int maxLen;
+
+    public LRUCache(int capacity) {
+        this.maxLen = capacity;
+        map = new LinkedHashMap<>();
+    }
+    
+    public int get(int key) {
+        if(!map.containsKey(key)) {
+            return -1;
+        }
+        int value = map.get(key);
+        map.remove(key);
+        map.putFirst(key,value);
+        return value;
+    }
+    
+    public void put(int key, int value) {
+        map.remove(key);
+        map.putFirst(key,value);
+        if (map.size() > maxLen) {
+            map.pollLastEntry();
+        }
+        
+    }
+}
